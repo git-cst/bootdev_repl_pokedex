2071b262095196e29044ac2ebe946b486cbc740c
csteenberg <csteenberg@hotmail.co.uk>
2025-03-09 12:03:32 +0100

    Make cache code

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,67 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type CacheEntry struct {
+	CreatedAt time.Time
+	Val       []byte
+}
+
+type Cache struct {
+	Entries  map[string]CacheEntry
+	Interval time.Duration
+	Mutex    sync.Mutex
+}
+
+func NewCache(key string, value []byte) *Cache {
+	newCache := &Cache{
+		Entries:  make(map[string]CacheEntry),
+		Interval: time.Duration(5 * time.Second),
+		Mutex:    sync.Mutex{},
+	}
+
+	newCache.reap()
+
+	return newCache
+}
+
+func (c *Cache) Add(key string, value []byte) {
+	c.Mutex.Lock()
+	defer c.Mutex.Unlock()
+
+	c.Entries[key] = CacheEntry{
+		CreatedAt: time.Now(),
+		Val:       value,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.Mutex.Lock()
+	defer c.Mutex.Unlock()
+
+	entry, exists := c.Entries[key]
+	if !exists {
+		return []byte{}, false
+	}
+
+	return entry.Val, true
+}
+
+func (c *Cache) reap() {
+	ticker := time.NewTicker(c.Interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		c.Mutex.Lock()
+		now := time.Now()
+		for key, entry := range c.Entries {
+			if now.Sub(entry.CreatedAt) > c.Interval {
+				delete(c.Entries, key)
+			}
+		}
+		c.Mutex.Unlock()
+	}
+}
